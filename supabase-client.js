ac0d4e9312c589e9e2305a2a209c6d92da8b7d05
Pushkar <pushkarjr7@gmail.com>
2026-05-12 18:07:06 +0530

    Finished frontend and added proper auth

--- supabase-client.js
+++ supabase-client.js
@@ -1,10 +1,10 @@
 // supabase-client.js
 
 // This is your correct REST API URL
-const SUPABASE_URL = 'https://wwicoxvylmseftwjflrx.supabase.co';
+const SUPABASE_URL = 'https://fglhsakunpkybtuiejck.supabase.co/rest/v1/';
 
 // Your Anon Key (this one is correct!)
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3aWNveHZ5bG1zZWZ0d2pmbHJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMjI5NjAsImV4cCI6MjA5Mzc5ODk2MH0.6CgZ-Js7ZH1G_xHReu7EcR2CRpJ3QXMVRq_a9nafZ3s';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnbGhzYWt1bnBreWJ0dWllamNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1ODUwODYsImV4cCI6MjA5NDE2MTA4Nn0.xxUvivKvqUg6diOQQPNIMFAEBCQxZHfzWhwzEdD7UBM';
 
 // Initialize the Supabase client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
